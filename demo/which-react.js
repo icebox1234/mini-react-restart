8bae8afadc2d1c49e8dc9f399b3db5c3e6d7308c
icebox1234 <464752812@qq.com>
2023-01-29 22:39:36 +0800

    init

---
+++ demo/which-react.js
@@ -0,0 +1,13 @@
+// import React, { useReducer, Component } from "react";
+// import ReactDOM from "react-dom";
+
+// import { useReducer } from "../src/react";
+import { Component } from 'react';
+/* "../src/react" */
+import ReactDOM from 'react-dom';
+/* "../src/react-dom" */
+export {
+  ReactDOM,
+  Component,
+  // , useReducer
+};
